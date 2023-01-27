14b3881b785517203bdb67616c1be5a509e00456
JashPrachodai <119839259+Jashprachodai@users.noreply.github.com>
2023-01-27 21:44:03 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int t=64+n;     for(int i=1;i<=n;i++,t--)     {         for(int j=i;j<=n;j++)         {             printf("%c ",t);         }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,35 +3,15 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int k,t;
-    for(int i=1;i<=n;i++)
+    int t=64+n;
+    for(int i=1;i<=n;i++,t--)
     {
-        for(int s=n-i;s>0;s--)
+        for(int j=i;j<=n;j++)
         {
-            printf(" ");
-        }
-        t=i-1;
-        k=0;
-        for(int j=1;j<=2*i-1;j++)
-        {
-            if(i==j)
-            {
-                t=0;
-                k++;
-            }
-            if(k==0)
-            {
-                printf("%d",t);
-                t--;
-            }
-            if(k!=0)
-            {
-                printf("%d",t);
-                t++;
-            }
-            
+            printf("%c ",t);
         }
         printf("
 ");
     }
+    
 }
