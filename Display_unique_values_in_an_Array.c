5a69e1389c90cff62d2460d034bb546afbfbfa3b
JashPrachodai <119839259+Jashprachodai@users.noreply.github.com>
2023-01-28 21:32:41 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int x[n];     for(int i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     int c,k=0;     for(int i=0;i<n;i++)     {         c=0;         for(int j=0;j<n;j++)         {             if(x[i]==x[j])             c++;         }         if(c==1)        {            printf("%d ",x[i]);            k++;        }     }     if(k==0)     printf("-1"); }

---
+++ Display_unique_values_in_an_Array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int x[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    int c,k=0;
+    for(int i=0;i<n;i++)
+    {
+        c=0;
+        for(int j=0;j<n;j++)
+        {
+            if(x[i]==x[j])
+            c++;
+        }
+        if(c==1)
+       {
+           printf("%d ",x[i]);
+           k++;
+       }
+    }
+    if(k==0)
+    printf("-1");
+}
